788e90cbf28e9257cb0c1a3c5fe7a477a0d506f4
ae2baker <ae2baker@gmail.com>
2023-04-25 19:57:42 +0000

    join and split

---
+++ lab.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+	name = input("What is your name? ")
+	day_of_week = input("What day of the week is it?\n> ")
+	print(f"Hello, {name}! Happy {day_of_week}!")
---
+++ no_shebang.py
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+print("ello!")
+print("Did you say, hello?")
+print("No, I said ello, but that\'s close enough.")
+
