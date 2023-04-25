9566e93e71132fd5811b755957beb451db04aac1
ae2baker <ae2baker@gmail.com>
2023-04-25 21:25:11 +0000

    manupulating lists

---
+++ listmethods/classlist.py
@@ -0,0 +1,10 @@
+#!/use/bin/env python3
+
+def list():
+    wordbank= ["indentation", "spaces"]
+    tlgstudents= ["Brandon", "Caleb", "Cat", "Chad the Beardulous", "Chance", "Chris", "Jessica", "Jorge", "Joshua", "Justin", "Lui", "Stephen"]
+
+print()
+wordbank.append(4)
+print()
+
