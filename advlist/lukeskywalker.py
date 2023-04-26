509c0b7fa271ed36ba9d4905b3bd0ffb1439564a
ae2baker <ae2baker@gmail.com>
2023-04-26 04:41:55 +0000

    lukeskywalker

---
+++ advlist/lukeskywalker.py
@@ -0,0 +1,10 @@
+luke_skywalker = {
+    "Title": ["Jedi Knight", "Jedi Master", "Red Five", "Rogue Leader"],
+    "Occupation": ["Apprentice", "Moisture Farmer", "Pilot", "Jedi"],
+    "Affiliation": ["Skywalker family", "Rebel Alliance", "Rogue Squadron", "Jedi", "New Republic", "New Jedi Order", "Resistance", "Galactic Alliance", "Jedi Council"],
+    "Family": ["Padmé Amidala (mother)", "Anakin Skywalker (father)", "Leia Organa (twin sister)", "Owen Lars (paternal step-uncle)", "Beru Lars (paternal step-aunt)", "Shmi Skywalker (paternal grandmother)", "Cliegg Lars (paternal step-grandfather)", "Han Solo (brother-in-law)", "Ben Solo (nephew)", "Jacen Solo (nephew)", "Jaina Solo (niece)", "Anakin Solo (nephew)", "Cade Skywalker (descendant)", "Luuke Skywalker (clone)"],
+    "Significant others": ["Nakari Kelen", "Tula Markona", "Callista Ming", "Gaeriel Captison", "Akanah Norand Goss Pell", "Shira Brie / Lumiya"],
+    "Master": ["Obi-Wan Kenobi", "Yoda"],
+    "Apprentice": ["Leia Organa", "New Jedi Order students", "Grogu", "Ben Solo", "Rey"]
+}
+
